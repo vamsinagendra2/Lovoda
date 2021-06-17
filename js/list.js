bf87be5a1c12b990e77123cb6852b5f6ccc0b4d4
vamsi <vamsinagendra2@gmail.com>
2021-06-17 19:55:31 +0530

    complete product page

---
+++ js/list.js
@@ -0,0 +1,192 @@
+var productsList = [
+        {
+            name: "Armored Curved Huggie Hoop Earrings",
+            category: "earrings",
+            sku: "010200865",
+            wrapping: "Options available",
+            color: "Gold",
+            material: "14k Gold Plating",
+            price: "28.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/1280x1280/products/6088/31576/010200865-2__83146.1619122623.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6088/31577/010200865-3__49996.1619122623.jpg?c=2"
+        },
+        {
+            name: "Ruffled Gold Drop Earrings",
+            category: "earrings",
+            sku: "010200878",
+            wrapping: "Options available",
+            color: "Black",
+            material: "Sterling Silver Post",
+            price: "18.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6087/31573/010200878-2__78431.1619122270.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6087/31575/010200878-3__70164.1619122266.jpg?c=2"
+        },
+        {
+            name: "Triangle and Pears Asymmetrical Hoop Earrings",
+            category: "earrings",
+            sku: "011000158",
+            wrapping: "Options available",
+            color: "Clear",
+            material: "14k Gold Plating",
+            price: "36.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6086/31572/011000158-2__91578.1619121962.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6086/31569/011000158-3__78715.1619121956.jpg?c=2"
+        },
+        {
+            name: "Tile Hoop Earrings | Emerald",
+            category: "earrings",
+            sku: "010200829-E",
+            wrapping: "Options available",
+            color: "Green",
+            material: "Sterling Silver Post",
+            price: "18.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6085/31568/010200829-E-2__37230.1619121680.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6085/31567/010200829-E-3__59379.1619121646.jpg?c=2"
+        },
+        {
+            name: "Pearl Cuff Earrings",
+            category: "earrings",
+            sku: "010700040",
+            wrapping: "Options available",
+            color: "White",
+            material: "",
+            price: "12.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6084/31563/010700040-S-1__84453.1619121359.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6084/31565/010700040-S-3__81770.1619121363.jpg?c=2"
+        },
+        {
+            name: "Ceres Button Earrings",
+            category: "earrings",
+            sku: "010200839",
+            wrapping: "Options available",
+            color: "Gold",
+            material: "Sterling Silver Post",
+            price: "16.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6083/31560/010200839-2__07753.1619120966.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6083/31562/010200839-3__90629.1619120971.jpg?c=2"
+        },
+        {
+            name: "Vintage Button Earrings",
+            category: "earrings",
+            sku: "010200872",
+            wrapping: "Options available",
+            color: "Gold",
+            material: "Sterling Silver Post",
+            price: "16.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6082/31557/010200872-2__93562.1619120587.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6082/31558/010200872-3__62940.1619120587.jpg?c=2"
+        },
+        {
+            name: "Tufted Huggie Hoop Earrings",
+            category: "earrings",
+            sku: "010200870",
+            wrapping: "Options available",
+            color: "Gold",
+            material: "14k Gold Plating",
+            price: "18.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6081/31553/010200870-2__22518.1619120262.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6081/31556/010200870-4__85590.1619120267.jpg?c=2"
+        },
+        {
+            name: "Sconce Drop Earrings",
+            category: "earrings",
+            sku: "010200854",
+            wrapping: "Options available",
+            color: "White",
+            material: "14k Gold Plating",
+            price: "25.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6080/31550/010200854-2__85176.1619120019.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6080/31551/010200854-3__79828.1619120019.jpg?c=2"
+        },
+        {
+            name: "Baguette Link Drop Earrings",
+            category: "earrings",
+            sku: "010200876",
+            wrapping: "Options available",
+            color: "Clear",
+            material: "14k Gold Plating",
+            price: "36.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6079/31549/010200876-2__52077.1619119725.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6079/31548/010200876-3__63573.1619119724.jpg?c=2"
+        },
+        {
+            name: "Heiress Huggie Hoop Earrings",
+            category: "earrings",
+            sku: "010200864",
+            wrapping: "Options available",
+            color: "Gold",
+            material: "14k Gold Plating",
+            price: "18.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6078/31378/010200864-2__84408.1618518818.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6078/31377/010200864-3__83677.1618518790.jpg?c=2"
+        },
+        {
+            name: "U Link Drop Earrings",
+            category: "earrings",
+            sku: "010200866",
+            wrapping: "Options available",
+            color: "Gold",
+            material: "14k Gold Plating",
+            price: "28.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6077/31374/010200866-2__24896.1618518489.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6077/31375/010200866-3__44398.1618518487.jpg?c=2"
+        },
+        {
+            name: "Refinement Hoop Earrings",
+            category: "earrings",
+            sku: "011000155",
+            wrapping: "Options available",
+            color: "Clear",
+            material: "14k Gold Plating",
+            price: "25.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6076/31370/011000155-1__07583.1618518209.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6076/31372/011000155-3__61270.1618518218.jpg?c=2"
+        },
+        {
+            name: "Grandeur Drop Earrings",
+            category: "earrings",
+            sku: "011000155",
+            wrapping: "Options available",
+            color: "Clear",
+            material: "14k Gold Plating",
+            price: "30.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6075/31367/010200879-2__71329.1618517918.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6075/31369/010200879-3__50704.1618517922.jpg?c=2"
+        },
+        {
+            name: "Cascading Stones Drop Earrings",
+            category: "earrings",
+            sku: "011000155",
+            wrapping: "Options available",
+            color: "Clear",
+            material: "14k Gold Plating",
+            price: "38.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6074/31523/010200851-6__29171.1619117953.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6074/31364/010200851-3__93988.1619117952.jpg?c=2"
+        },
+        // neck products................
+        {
+            name: "By The Sea Charm Necklace | Gold (14K)",
+            category: "",
+            sku: "011000155",
+            wrapping: "Options available",
+            color: "Gold",
+            material: "14k Gold Plating",
+            price: "36.00",
+            url: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6025/30242/020600155-G-2__27222.1612985571.jpg?c=2",
+            url2: "https://cdn11.bigcommerce.com/s-8pcbviyy/images/stencil/600x900/products/6025/30241/020600155-G-3__09175.1612985537.jpg?c=2"
+        },
+        {
+            name: "",
+            category: "",
+            sku: "011000155",
+            wrapping: "Options available",
+            color: "",
+            material: "",
+            price: "38.00",
+            url: "",
+            url2: ""
+        }
+]
+    
+localStorage.setItem('productsList', JSON.stringify(productsList));
